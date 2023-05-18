595bca45bed7f3c19f3d0416ac8a49eec93a5071
Viktoria Smirnova <vssmirnova96@gmail.com>
2023-05-18 15:34:09 +0300

    save

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        int check = 50;
+        int x = 100;
+        int sum = 500;
+        int result;
+        int bonus;
+        if (sum >= 1000) {
+            bonus = sum / x;
+        } else {
+            bonus = 0;
+        }
+        if (sum >= 1000) {
+            result = check + sum + bonus;
+        } else {
+            result = sum + check;
+        }
+        System.out.println(result);
+        System.out.println(bonus);
 
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
